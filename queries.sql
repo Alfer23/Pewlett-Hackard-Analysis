72b4408a919b53b20756ed18944790f92a32b2fc
Alexis Fernando Santiago Resendiz <feralexis51@gmail.com>
2021-11-21 06:02:12 -0600

    modified queries

--- queries.sql
+++ queries.sql
@@ -232,3 +232,30 @@ INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no);
 
 SELECT * FROM emp_info2;
+
+-- Create a Retirement titles table
+SELECT 	e.emp_no,
+		e.first_name,
+		e.last_name,
+		ti.title,
+		ti.from_date,
+		ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+SELECT * FROM retirement_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no) 	rt.emp_no,
+		rt.first_name,
+		rt.last_name,
+		rt.title
+INTO unique_retire_titles
+FROM retirement_titles AS rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+SELECT * FROM unique_retire_titles;
